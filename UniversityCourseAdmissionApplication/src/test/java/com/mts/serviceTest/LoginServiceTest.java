8a90d655182e85069d9302a97447b494655cd8dc
emchandrakant <chandrakantshinde600@gmail.com>
2022-07-09 15:05:43 +0530

    Login Test completed..

--- UniversityCourseAdmissionApplication/src/test/java/com/mts/serviceTest/LoginServiceTest.java
+++ UniversityCourseAdmissionApplication/src/test/java/com/mts/serviceTest/LoginServiceTest.java
@@ -1,6 +1,9 @@
 package com.mts.serviceTest;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -10,6 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import com.mts.entities.Admission;
+import com.mts.entities.AdmissionCommiteeMember;
+import com.mts.entities.AdmissionStatus;
+import com.mts.entities.Applicant;
+import com.mts.entities.UniversityStaffMember;
 import com.mts.exception.AdmissionMemNotFoundException;
 import com.mts.exception.ApplicantNotFoundException;
 import com.mts.exception.StaffMemberNotFoundException;
@@ -37,24 +45,30 @@ public class LoginServiceTest {
 	int applicantId=101;
 	String password="pass";
 	
+	AdmissionStatus status=AdmissionStatus.Applied;
+	
+	Applicant c1= new Applicant(101,"Sagar","7934873784","EE",59,new Admission(),status,"pass");
+	AdmissionCommiteeMember d1= new AdmissionCommiteeMember(101,"Sagar","7934873784","pass");
+	UniversityStaffMember e1= new UniversityStaffMember(101,"pass","Electrical Engineer");
+	
 	@Test
 	@Order(1)
 	public void testApplicant() throws ApplicantNotFoundException {
-		//when(service.loginAsApplicant(applicantId, password)).thenReturn(false);
+		when(applicantRepo.findById(applicantId)).thenReturn(Optional.of(c1));
 		assertTrue(service.loginAsApplicant(applicantId, password));
 	}
 	
 	@Test
 	@Order(2)
 	public void testAdmissionCommitee() throws AdmissionMemNotFoundException {
-		//when(service.loginAsApplicant(applicantId, password)).thenReturn(false);
+		when(commiteeRepo.findById(applicantId)).thenReturn(Optional.of(d1));
 		assertTrue(service.loginAsAdmissionCommiteeMember(applicantId, password));
 	}
 	
 	@Test
 	@Order(3)
 	public void testStaff() throws StaffMemberNotFoundException {
-		//when(service.loginAsApplicant(applicantId, password)).thenReturn(false);
+		when(staffRepo.findById(applicantId)).thenReturn(Optional.of(e1));
 		assertTrue(service.loginAsUniversityStaffMember(applicantId, password));
 	}
 }
